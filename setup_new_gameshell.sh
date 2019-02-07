803122eb27ce9ffa8f89bd25f5b45e92943cf15a
hpcodecraft <thesquidpeople@googlemail.com>
2019-02-07 22:51:05 +0100

    Adds common aliases and screenshot tools to setup

--- setup_new_gameshell.sh
+++ setup_new_gameshell.sh
@@ -22,6 +22,36 @@ sudo apt-get update
 
 MENU_CHOICE="0"
 
+printf "${GREEN}Install bash aliases to ~/.bash_aliases? (y/N): ${NC}"
+read MENU_CHOICE
+
+if [ "${MENU_CHOICE}" == "y" ]
+then
+    wget -O ~/.bash_aliases https://raw.githubusercontent.com/hpcodecraft/Gameshell/master/system/.bash_aliases
+else
+    echo "Skipped"
+fi
+
+MENU_CHOICE="0"
+
+printf "${GREEN}Install SSH screenshot tools? (y/N): ${NC}"
+read MENU_CHOICE
+
+if [ "${MENU_CHOICE}" == "y" ]
+then
+    mkdir ~/screenshots
+    sudo apt-get install imagemagick
+    echo "" >> ~/.bash_aliases
+    echo "# alias for taking screenshots" >> ~/.bash_aliases
+    echo "export DISPLAY=:0" >> ~/.bash_aliases
+    echo 'alias take_screenshot="xwd -root | convert xwd:-"' >> ~/.bash_aliases
+    echo "" >> ~/.bash_aliases
+    echo "Done! You can now take screenshots via SSH by running 'take_screenshot <filename>'"
+    echo "e.g. 'take_screenshot ~/screenshots/screenshot.png'"
+else
+    echo "Skipped"
+fi
+
 printf "${GREEN}Update and configure retroarch? (y/N): ${NC}"
 read MENU_CHOICE
 
