80d8c9b764f06e216265622830834150de5b736c
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2019-02-18 21:04:10 +0100

    Add git prompt, copy files instead of downloading them

--- setup_new_gameshell.sh
+++ setup_new_gameshell.sh
@@ -4,15 +4,26 @@ GREEN='\033[1;32m'
 NC='\033[0m'
 
 HERE=`pwd`
+MENU_CHOICE="0"
 
 printf "${GREEN}**********************************************${NC}\n"
+printf "${GREEN}*                                            *${NC}\n"
 printf "${GREEN}* Welcome!                                   *${NC}\n"
-printf "${GREEN}* This installer will ask you some questions *${NC}\n"
+printf "${GREEN}*                                            *${NC}\n"
+printf "${GREEN}* This installer will now update the lists   *${NC}\n"
+printf "${GREEN}* of packages known to the GameShell         *${NC}\n"
+printf "${GREEN}*                                            *${NC}\n"
+printf "${GREEN}* Afterwards, it will ask you some questions *${NC}\n"
+printf "${GREEN}*                                            *${NC}\n"
 printf "${GREEN}* Type 'y' to accept                         *${NC}\n"
 printf "${GREEN}* or hit any other key to skip               *${NC}\n"
+printf "${GREEN}*                                            *${NC}\n"
 printf "${GREEN}**********************************************${NC}\n\n"
 
-MENU_CHOICE="0"
+sleep 10
+
+printf "${GREEN}Updating package lists...${NC}\n"
+sudo apt-get update
 
 printf "${GREEN}Resize the root partition? (y/N): ${NC}"
 read MENU_CHOICE
@@ -24,22 +35,17 @@ else
     echo "Skipped"
 fi
 
-printf "${GREEN}Updating package lists...${NC}\n"
-sudo apt-get update
-
-MENU_CHOICE="0"
 
 printf "${GREEN}Install bash aliases to ~/.bash_aliases? (y/N): ${NC}"
 read MENU_CHOICE
 
 if [ "${MENU_CHOICE}" == "y" ]
 then
-    wget -O ${HOME}/.bash_aliases https://raw.githubusercontent.com/hpcodecraft/gameshell-setup/master/system/.bash_aliases
+    cp system/bash_aliases ${HOME}/.bash_aliases
 else
     echo "Skipped"
 fi
 
-MENU_CHOICE="0"
 
 printf "${GREEN}Install SSH screenshot tools? (y/N): ${NC}"
 read MENU_CHOICE
@@ -60,6 +66,22 @@ else
     echo "Skipped"
 fi
 
+
+printf "${GREEN}Show Git branch in shell prompt? (y/N): ${NC}"
+read MENU_CHOICE
+
+if [ "${MENU_CHOICE}" == "y" ]
+then
+    cp system/gitprompt ${HOME}/.gitprompt
+    OUT_FILE="${HOME}/.bashrc"
+    echo "" >> $OUT_FILE
+    echo "source ${HOME}/.gitprompt" >> $OUT_FILE
+    echo "" >> $OUT_FILE
+    printf "\n${GREEN}Done! The current git branch will now be displayed in the prompt'${NC}\n\n"
+else
+    echo "Skipped"
+fi
+
 printf "${GREEN}Update and configure retroarch? (y/N): ${NC}"
 read MENU_CHOICE
 
@@ -70,7 +92,6 @@ else
     echo "Skipped"
 fi
 
-MENU_CHOICE="0"
 CLONED_INSTALLERS=0
 INSTALLER_DIR="${HOME}/mods/gameshell-installers"
 
@@ -121,6 +142,7 @@ cd ${HERE}
 
 printf "\n${GREEN}**********************************************${NC}\n"
 printf "${GREEN}* Finished!                                  *${NC}\n"
+printf "${GREEN}*                                            *${NC}\n"
 printf "${GREEN}* Please reload the launcher                 *${NC}\n"
 printf "${GREEN}* ...and have fun with your Gameshell!       *${NC}\n"
 printf "${GREEN}**********************************************${NC}\n\n"
